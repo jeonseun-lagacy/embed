a5e8eb3283e0fc54cd85d40e342793de73bb79b4
yongjjang <pastelhawk@gmail.com>
2020-06-25 13:39:48 +0900

    Update webstreaming.py

--- embed_camera/webstreaming.py
+++ embed_camera/webstreaming.py
@@ -1,4 +1,6 @@
+# 웹 스트리밍에 대한 인터넷 예제를 참고하였습니다. 움직임을 탐지하면 탐지된 부분에 바운딩 박스를 그려줍니다.
 from pyimagesearch.motion_detection import SingleMotionDetector
+
 from imutils.video import VideoStream
 from flask import Response
 from flask import Flask
@@ -18,23 +20,30 @@ app = Flask(__name__)
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
-
+# 테스트용 페이지
 @app.route("/")
 def index():
 	return render_template("index.html")
 
 
+# 
 def detect_motion(frameCount):
 	global vs, outputFrame, lock
+	# 가중치 파일을 사용한 움직임 검출 클래스 
 	md = SingleMotionDetector(accumWeight=0.1)
 	total = 0
 
 	while True:
 		frame = vs.read()
 		frame = imutils.resize(frame, width=400)
+		
+		# 영상 처리가 용이한 Gray 스케일로 변환
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		
+		# 엣지 검출을 통한 빠른 모션 감지를 위해 선형 필터인 가우시안 필터 적용.
 		gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
+		
+		# 타임 스탬프를 화면 좌측 하단에 출력
 		timestamp = datetime.datetime.now()
 		cv2.putText(frame, timestamp.strftime(
 			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
@@ -55,7 +64,9 @@ def detect_motion(frameCount):
 			outputFrame = frame.copy()
 		
 
+# 예제 참고, 제너레이터 함수 함수 작성
 def generate():
+	# 쓰레드 활용을 위한 lock 선언
 	global outputFrame, lock
 
 	while True:
@@ -67,7 +78,8 @@ def generate():
 
 			if not flag:
 				continue
-
+		
+		# yield 키워드를 사용해 웹 페이지 스트리밍
 		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
 			bytearray(encodedImage) + b'\r\n')
 
